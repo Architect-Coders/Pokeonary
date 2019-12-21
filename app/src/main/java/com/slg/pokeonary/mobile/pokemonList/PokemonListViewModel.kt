e277b48ccd5d57477aa930553ef9467ecc94fa68
Sergi Llamas <sergi.llamas@tiendeo.com>
2019-12-21 20:31:33 +0100

    Use ViewModel instead of Presenter

---
+++ app/src/main/java/com/slg/pokeonary/mobile/pokemonList/PokemonListViewModel.kt
@@ -0,0 +1,61 @@
+package com.slg.pokeonary.mobile.pokemonList
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.slg.pokeonary.data.repository.pokemon.PokemonDataRepository
+import com.slg.pokeonary.data.repository.pokemon.dataSource.remote.PokemonRemoteDataSource
+import com.slg.pokeonary.domain.pokemon.useCase.GetPokemonList
+import com.slg.pokeonary.domain.pokemon.useCase.GetPokemonListParams
+import com.slg.pokeonary.mobile.pokemonList.model.PokemonViewEntity
+import com.slg.pokeonary.mobile.pokemonList.model.transformToUi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+class PokemonListViewModel(private val context: Context) : CoroutineScope, ViewModel() {
+
+    private val job = SupervisorJob()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    sealed class UiModel {
+        class Content(val pokemons: List<PokemonViewEntity>) : UiModel()
+    }
+
+    private val _model = MutableLiveData<UiModel>()
+    val model: LiveData<UiModel>
+        get() {
+            if (_model.value == null) refresh()
+            return _model
+        }
+
+    private fun refresh() {
+        val getPokemonListUseCase =
+            GetPokemonList(PokemonDataRepository(PokemonRemoteDataSource(context)))
+        launch {
+            val pokemons = getPokemonListUseCase.buildAsync(GetPokemonListParams(START, COUNT))
+            _model.value = UiModel.Content(pokemons.transformToUi())
+        }
+    }
+
+    override fun onCleared() {
+        job.cancel()
+        super.onCleared()
+    }
+
+    companion object {
+        const val START = 0
+        const val COUNT = 20
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    class PokemonListViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+            PokemonListViewModel(context) as T
+    }
+}
