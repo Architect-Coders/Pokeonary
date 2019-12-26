c9ea2acd703b6129347071080ea31110242b1dbc
Sergi Llamas <shigeru.yamasaki.17@gmail.com>
2019-12-26 12:47:09 +0100

    Build correct url in ui model

--- app/src/main/java/com/slg/pokeonary/mobile/pokemonList/model/PokemonViewEntity.kt
+++ app/src/main/java/com/slg/pokeonary/mobile/pokemonList/model/PokemonViewEntity.kt
@@ -2,11 +2,20 @@ package com.slg.pokeonary.mobile.pokemonList.model
 
 import com.slg.pokeonary.domain.pokemon.model.Pokemon
 
-class PokemonViewEntity (
+class PokemonViewEntity(
     val name: String?,
     val imageUrl: String?
 )
 
-private fun Pokemon.transformToUi() = PokemonViewEntity(name, imageUrl)
+private fun Pokemon.transformToUi() = PokemonViewEntity(name, getPokemonFrontSpriteFromUrl(url))
 
 fun List<Pokemon>.transformToUi() = map { it.transformToUi() }
+
+private fun getPokemonFrontSpriteFromUrl(url: String?) =
+    url?.run {
+        val splittedUrl = split("/")
+        val pokemonNumber = splittedUrl.lastOrNull { !it.isBlank() }
+        pokemonNumber?.let {
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$pokemonNumber.png"
+        }
+    }
