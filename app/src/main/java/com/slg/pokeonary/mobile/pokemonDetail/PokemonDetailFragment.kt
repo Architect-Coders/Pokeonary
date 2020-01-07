027b2ea692db5cf59510db1b62cb83da781df5e0
Sergi Llamas <shigeru.yamasaki.17@gmail.com>
2020-01-07 17:53:00 +0100

    Create detail fragment

---
+++ app/src/main/java/com/slg/pokeonary/mobile/pokemonDetail/PokemonDetailFragment.kt
@@ -0,0 +1,28 @@
+package com.slg.pokeonary.mobile.pokemonDetail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.slg.pokeonary.R
+import com.slg.pokeonary.mobile.common.inflate
+
+class PokemonDetailFragment : Fragment() {
+
+    private val args: PokemonDetailFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return container?.inflate(R.layout.fragment_pokemon_detail)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Toast.makeText(context, args.pokemonName, Toast.LENGTH_LONG).show()
+    }
+}
