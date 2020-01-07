bf73bde88ff1db46bc8996e9d2b5db5742948508
Sergi Llamas <shigeru.yamasaki.17@gmail.com>
2020-01-07 17:52:15 +0100

    Add event for navigation and initialize it

--- app/src/main/java/com/slg/pokeonary/mobile/pokemonList/PokemonListFragment.kt
+++ app/src/main/java/com/slg/pokeonary/mobile/pokemonList/PokemonListFragment.kt
@@ -8,6 +8,8 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.slg.pokeonary.R
 import com.slg.pokeonary.mobile.common.inflate
@@ -18,6 +20,7 @@ import kotlinx.android.synthetic.main.activity_pokemon_list.*
 
 class PokemonListFragment : Fragment() {
 
+    private lateinit var navController: NavController
     private lateinit var viewModel: PokemonListViewModel
     private lateinit var pokemonsAdapter: PokemonsAdapter
 
@@ -30,22 +33,35 @@ class PokemonListFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        navController = view.findNavController()
+
         viewModel = ViewModelProviders.of(
             this,
             PokemonListViewModel.PokemonListViewModelFactory(activity!!.application)
         )[PokemonListViewModel::class.java]
 
         pokemonsAdapter = PokemonsAdapter()
+
         initializeRecyclerView()
 
         viewModel.model.observe(this, Observer(::updateUi))
+        viewModel.navigation.observe(this, Observer { event ->
+            event.getContentIfNotHandled()?.let { pokemonName ->
+                // TODO
+            }
+        })
     }
 
     private fun initializeRecyclerView() {
         recyclerView.adapter = pokemonsAdapter
         val dividerDrawable = ContextCompat.getDrawable(context!!, R.drawable.recycler_view_divider)
-        recyclerView.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
-            .apply { dividerDrawable?.let { setDrawable(it) } })
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                DividerItemDecoration.VERTICAL
+            ).apply { dividerDrawable?.let { setDrawable(it) } }
+        )
     }
 
     private fun updateUi(uiModel: PokemonListViewModel.UiModel) {
